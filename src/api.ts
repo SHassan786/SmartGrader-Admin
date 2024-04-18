b94714fe805499831833c6ee2672c3b466a0b34e
Shehryar Hassan <74443334+SHassan786@users.noreply.github.com>
2024-04-18 20:24:38 +0000

    delete Quiz Api implemented and dashboard Cards updated

--- src/api.ts
+++ src/api.ts
@@ -10,7 +10,9 @@ export const API_URLS = {
     updateClass: `${host}/class`,
     getQuizzes: `${host}/quiz/getQuizzesForTeacher`,
     updateQuiz: `${host}/quiz`,
+    deleteQuiz: `${host}/quiz//deleteQuiz`,
     getQuestionsByQuizId: `${host}/question/getQuestionsByQuizId`,
     createQuestion: `${host}/question/createQuestionsByQuizId`,
     updateQuestion: `${host}/question/updateQuestion`,
+    deleteQuestion: `${host}/question/deleteQuestion`,
 } as const;
